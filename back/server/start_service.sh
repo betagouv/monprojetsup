c12fde77dd7b3b5f0bc66f68e861b90cdc158309
Hugo Gimbert <hugo.gimbert@beta.gouv.fr>
2024-02-17 00:07:52 +0100

    rebase

--- back/server/start_service.sh
+++ back/server/start_service.sh
@@ -2,12 +2,4 @@
 cd /home/gimbert/code/monprojetsup/back/java/tmp
 
 #java -jar -Xms16G -Xmx16G /home/gimbert/code/monprojetsup/back/java/target/monprojetsup-1.4.0-exec.jar
-java -jar -Xms16G -Xmx16G -XX:+ExitOnOutOfMemoryError /home/gimbert/code/monprojetsup/back/java/target/monprojetsup-1.4.0-exec.jar
-
-&
-
-# Get the PID of the last background process
-PID=$!
-
-# Write the PID to a file
-echo $PID > /home/gimbert/mps.pid
+java -jar -Xms16G -Xmx16G -XX:+ExitOnOutOfMemoryError /home/gimbert/code/monprojetsup/back/java/target/monprojetsup-1.4.0-exec.jar 
