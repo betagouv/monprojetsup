ad51145c8746ef7f07aa10aadf409261afc3fdf9
Hugo Gimbert <hugo.gimbert@cnrs.fr>
2024-02-17 00:07:02 +0100

    store pid file at atsraup

--- back/java/src/main/kotlin/fr/gouv/monprojetsup/Application.kt
+++ back/java/src/main/kotlin/fr/gouv/monprojetsup/Application.kt
@@ -11,6 +11,9 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
 
 
 @SpringBootApplication
@@ -32,12 +35,34 @@ const val BASE_PATH = "/api/$API_VERSION"
  * Main function
  */
 fun main(args: Array<String>) {
+
+    writePidtoFile()
     val app = SpringApplication(Application::class.java)
     val context = app.run(*args)
     val webServer = context.getBean(WebServer::class.java)
 
     val loadOnlyDB = false;
     webServer.init(loadOnlyDB)
+
+}
+
+fun writePidtoFile() {
+    // Get the current process handle
+    val currentProcess = ProcessHandle.current()
+
+    // Get the PID of the current process
+    val pid = currentProcess.pid()
+
+    // Define the path to the file where the PID should be written
+    val filePath = "mps.pid"
+
+    try {
+        // Write the PID to the file
+        Files.writeString(Paths.get(filePath), pid.toString())
+        println("Successfully wrote PID to file: $filePath")
+    } catch (e: IOException) {
+        System.err.println("An error occurred while writing the PID to file: ")
+    }
 
 }
 
