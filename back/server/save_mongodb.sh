a4fa17522390968db9657804a79b7b452ca34b04
Hugo Gimbert <hugo.gimbert@beta.gouv.fr>
2024-02-17 21:56:01 +0100

    scripts suivi

--- back/server/save_mongodb.sh
+++ back/server/save_mongodb.sh
@@ -5,7 +5,7 @@ tar -czf dump.tgz dump
 
 mvn compile exec:java -f../pom.xml -DDB_NAME=${DB_NAME} -DDB_PEM=${DB_PEM} -DDB_URI=${DB_URI} -DGOOGLE_OAUTH2_CLIENT_ID=${GOOGLE_OAUTH2_CLIENT_ID} -DGOOGLE_OAUTH2_CLIENT_SECRET=${GOOGLE_OAUTH2_CLIENT_SECRET} -DskipTests=true -Dexec.mainClass=fr.gouv.monprojetsup.web.db.admin.ExportTracesAndClearFromDB
 now=$(date +"%Y_%m_%d_%I_%M_%p")
-cp traces.json traces_$now.json
-tar -czf traces.tgz traces_$now.json
+cp traces.json data/traces_$now.json
+tar -czf traces.tgz data/traces_$now.json
 
 popd
