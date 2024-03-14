9bdaa9389efbc786b79aee34dcc339705a374f18
Hugo Gimbert <hugo.gimbert@cnrs.fr>
2024-03-14 12:54:45 +0100

    removed legacy files

--- front/src/php/back_labri/settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-error_reporting(E_ALL);
-
-//$BACKEND_URL="http://parcoursup-demos.labri.fr:8000/";
-$BACKEND_URL="http://localhost:8000/";
-//$BACKEND_URL="http://5.196.74.114:8000/";
-?>
--- front/src/php/front_labri/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-error_reporting(E_ALL);
-
-$BACKEND_URL="http://147.210.13.4/orientation/service.php";
-$BACKEND_URL2="http://parcoursup-demos.labri.fr/orientation/service.php";
-?>
