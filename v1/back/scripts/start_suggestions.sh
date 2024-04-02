4f40bb1a8fad7dd922fcdbdd07a2d078b41470a0
Hugo Gimbert <hugo.gimbert@beta.gouv.fr>
2024-04-02 21:30:18 +0200

    updated scripts

--- v1/back/scripts/start_suggestions.sh
+++ v1/back/scripts/start_suggestions.sh
@@ -1,1 +1,4 @@
-mvn clean compile exec:java -f../suggestions/pom.xml -Dexec.mainClass="fr.gouv.monprojetsup.app.ApplicationSuggestionsKt"
+#!/bin/bash
+mvn clean install -f../common/pom.xml
+mvn clean install -f../etl/pom.xml
+mvn clean compile exec:java -f../suggestions/pom.xml -Dexec.mainClass="fr.gouv.monprojetsup.suggestions.ApplicationSuggestionsKt"
