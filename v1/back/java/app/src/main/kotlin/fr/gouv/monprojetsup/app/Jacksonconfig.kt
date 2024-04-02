537faa07202fc6406013e5758eabb0cc083a690a
Hugo Gimbert <hugo.gimbert@cnrs.fr>
2024-04-02 21:46:11 +0200

    added ignorable fields 2

--- v1/back/java/app/src/main/kotlin/fr/gouv/monprojetsup/app/Jacksonconfig.kt
+++ v1/back/java/app/src/main/kotlin/fr/gouv/monprojetsup/app/Jacksonconfig.kt
@@ -3,6 +3,7 @@ package fr.gouv.monprojetsup.app
 import com.fasterxml.jackson.annotation.JsonAutoDetect
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.PropertyAccessor
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -17,7 +18,7 @@ class JacksonConfig {
         objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        
+
         return objectMapper
     }
 }
