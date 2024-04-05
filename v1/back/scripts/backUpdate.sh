c0e7c80d8e8be4d23487e219c51214d12ff808b8
Hugo Gimbert <hugo.gimbert@beta.gouv.fr>
2024-04-05 10:41:30 +0200

    script

--- v1/back/scripts/backUpdate.sh
+++ v1/back/scripts/backUpdate.sh
@@ -1,4 +1,6 @@
 git pull
+mvn -f ../common/pom.xml -DskipTests=true clean compile install
+mvn -f ../etl/pom.xml -DskipTests=true clean compile install
 mvn -f ../pom.xml -DskipTests=true clean compile package
 #mvn -f ../pom.xml -DskipTests=true -P standalone compile install
 sudo service mps restart
