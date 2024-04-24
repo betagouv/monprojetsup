ef9162e6a05b14d9e19fc6f7e98a8219de1eb983
Aurelia Del Zotto <aurelia.del.zotto@octo.com>
2024-04-24 10:34:29 +0200

    chore : Fix swagger

--- app/back/src/main/fr/gouv/monprojetsup/MonProjetSupApplication.kt
+++ app/back/src/main/fr/gouv/monprojetsup/MonProjetSupApplication.kt
@@ -1,8 +1,11 @@
 package fr.gouv.monprojetsup
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@OpenAPIDefinition(servers = [Server(url = "/")])
 @SpringBootApplication
 class MonProjetSupApplication
 
