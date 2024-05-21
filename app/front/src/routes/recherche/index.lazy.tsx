6f134af1f5d505b250cf357ad98dde52d07a5c28
Hugo Gimbert <Numero7@users.noreply.github.com>
2024-05-21 16:00:44 +0200

    restored app with git restore

--- app/front/src/routes/recherche/index.lazy.tsx
+++
@@ -1,6 +0,0 @@
-import RecherchePage from "@/features/recherche/ui/RecherchePage/RecherchePage";
-import { createLazyFileRoute } from "@tanstack/react-router";
-
-export const Route = createLazyFileRoute("/recherche/")({
-  component: RecherchePage,
-});
