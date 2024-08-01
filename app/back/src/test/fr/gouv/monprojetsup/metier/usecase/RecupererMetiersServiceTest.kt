3f0301e692c99db7d96b49d6e79fdb0ce65ebc16
Aurelia Del Zotto <aurelia.del.zotto@octo.com>
2024-08-01 16:41:49 +0200

    feat(179) : Récupérer le détails de plusieurs métiers

---
+++ app/back/src/test/fr/gouv/monprojetsup/metier/usecase/RecupererMetiersServiceTest.kt
@@ -0,0 +1,88 @@
+package fr.gouv.monprojetsup.metier.usecase
+
+import fr.gouv.monprojetsup.commun.lien.domain.entity.Lien
+import fr.gouv.monprojetsup.metier.domain.entity.Metier
+import fr.gouv.monprojetsup.metier.domain.port.MetierRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.BDDMockito.given
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+class RecupererMetiersServiceTest {
+    @Mock
+    private lateinit var metierRepository: MetierRepository
+
+    @InjectMocks
+    private lateinit var recupererMetiersService: RecupererMetiersService
+
+    @BeforeEach
+    fun setup() {
+        MockitoAnnotations.openMocks(this)
+    }
+
+    @Test
+    fun `doit retourner la liste des métiers`() {
+        // Given
+        val idsMetier = listOf("MET_356", "MET_355", "MET_358")
+        val metiers =
+            listOf(
+                Metier(
+                    id = "MET_356",
+                    nom = "ingénieur / ingénieure matériaux",
+                    descriptif =
+                        "<p>L'ingénieur matériaux intervient de la conception à l'utilisation des matériaux. À la pointe de " +
+                            "l'innovation, cet expert met ses compétences au service d'un bureau d'études, d'une entreprise " +
+                            "industrielle ou d'un organisme de recherche.</p>",
+                    liens =
+                        listOf(
+                            Lien(
+                                nom = "Management et ingénierie études, recherche et développement industriel",
+                                url = "https://candidat.pole-emploi.fr/marche-du-travail/fichemetierrome?codeRome=H1206",
+                            ),
+                            Lien(
+                                nom = "ingénieur / ingénieure matériaux",
+                                url = "https://explorer-avenirs.onisep.fr/http/redirection/metier/slug/MET.356",
+                            ),
+                        ),
+                ),
+                Metier(
+                    id = "MET_355",
+                    nom = "responsable de laboratoire de contrôle en chimie",
+                    descriptif =
+                        "<p> Dernière étape avant la mise sur le marché d'un produit issu de l'industrie de la chimie : son " +
+                            "contrôle. Sous la direction du responsable de laboratoire de contrôle, des tests sont effectués pour " +
+                            "évaluer sa qualité et sa conformité aux normes. </p>",
+                    liens = emptyList(),
+                ),
+                Metier(
+                    id = "MET_358",
+                    nom = "pilote d'hélicoptère",
+                    descriptif =
+                        "<p>Le pilote d'hélicoptère, militaire ou civil, participe à des missions de combat ou de secours. Il " +
+                            "est capable de se poser n'importe où et d'évoluer en montagne. Sang-froid et ténacité sont nécessaires " +
+                            "pour exercer ce métier.</p>",
+                    liens =
+                        listOf(
+                            Lien(
+                                nom = "pilote d'hélicoptère",
+                                url = "https://explorer-avenirs.onisep.fr/http/redirection/metier/slug/MET.358",
+                            ),
+                            Lien(
+                                nom = "Pilotage et navigation technique aérienne",
+                                url = "https://candidat.pole-emploi.fr/marche-du-travail/fichemetierrome?codeRome=N2102",
+                            ),
+                        ),
+                ),
+            )
+        given(metierRepository.recupererLesMetiersDetailles(idsMetier)).willReturn(metiers)
+
+        // When
+        val resultat = recupererMetiersService.recupererMetiers(idsMetier)
+
+        // Then
+        assertThat(resultat).isEqualTo(metiers)
+    }
+}
