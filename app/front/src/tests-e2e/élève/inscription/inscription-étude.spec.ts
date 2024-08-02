e1afcd2370876a24324da23e130060934f113bae
Maxime <maxime.vigier@octo.com>
2024-08-02 15:13:02 +0200

    fix: case sensitive issue fixed

--- app/front/src/tests-e2e/élève/inscription/inscription-étude.spec.ts
+++ app/front/src/tests-e2e/élève/inscription/inscription-étude.spec.ts
@@ -1,4 +1,4 @@
-import { InscriptionTestHelper } from "./t";
+import { InscriptionTestHelper } from "./inscriptionTestHelper";
 import { i18n } from "@/configuration/i18n/i18n";
 import { expect, type Page, test } from "@playwright/test";
 
