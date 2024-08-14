e30e9ad0600b4f8e8afbc932a258c086b613955d
Aurelia Del Zotto <aurelia.del.zotto@octo.com>
2024-08-14 14:40:04 +0200

    feat(166) : Ajout de la pagination sur les endpoints métiers

--- app/back/src/main/fr/gouv/monprojetsup/metier/application/dto/MetiersCourtsDTO.kt
+++ app/back/src/main/fr/gouv/monprojetsup/metier/application/dto/MetiersCourtsDTO.kt
@@ -1,5 +1,7 @@
 package fr.gouv.monprojetsup.metier.application.dto
 
+import fr.gouv.monprojetsup.commun.hateoas.application.dto.PaginationDTO
+
 data class MetiersCourtsDTO(
     val metiers: List<MetierCourtDTO>,
-)
+) : PaginationDTO()
--- app/back/src/test/fr/gouv/monprojetsup/metier/application/controller/MetierControllerTest.kt
+++ app/back/src/test/fr/gouv/monprojetsup/metier/application/controller/MetierControllerTest.kt
@@ -4,6 +4,9 @@ import fr.gouv.monprojetsup.commun.ConnecteAvecUnEleve
 import fr.gouv.monprojetsup.commun.ConnecteAvecUnEnseignant
 import fr.gouv.monprojetsup.commun.ConnecteSansId
 import fr.gouv.monprojetsup.commun.application.controller.ControllerTest
+import fr.gouv.monprojetsup.commun.erreur.domain.MonProjetSupBadRequestException
+import fr.gouv.monprojetsup.commun.hateoas.domain.entity.Hateoas
+import fr.gouv.monprojetsup.commun.hateoas.usecase.HateoasBuilder
 import fr.gouv.monprojetsup.commun.lien.domain.entity.Lien
 import fr.gouv.monprojetsup.metier.domain.entity.Metier
 import fr.gouv.monprojetsup.metier.domain.entity.MetierCourt
@@ -31,6 +34,9 @@ class MetierControllerTest(
 
     @MockBean
     lateinit var rechercherMetiersService: RechercherMetiersService
+
+    @MockBean
+    lateinit var hateoasBuilder: HateoasBuilder
 
     @Nested
     inner class `Quand on appelle la route de récupération de métiers` {
@@ -90,6 +96,15 @@ class MetierControllerTest(
                     ),
                 )
             `when`(recupererMetiersService.recupererMetiers(idsMetier)).thenReturn(metiers)
+            val hateoas =
+                Hateoas(
+                    pageActuelle = 1,
+                    pageSuivante = null,
+                    premierePage = 1,
+                    dernierePage = 1,
+                    listeCoupee = metiers,
+                )
+            `when`(hateoasBuilder.creerHateoas(liste = metiers, numeroDePageActuelle = 1, tailleLot = 30)).thenReturn(hateoas)
 
             // When & Then
             mvc.perform(
@@ -135,6 +150,20 @@ class MetierControllerTest(
                                   "url": "https://candidat.pole-emploi.fr/marche-du-travail/fichemetierrome?codeRome=N2102"
                                 }
                               ]
+                            }
+                          ],
+                          "liens": [
+                            {
+                              "rel": "premier",
+                              "href": "http://localhost/api/v1/metiers?ids=MET_356&ids=MET_355&ids=MET_358&numeroDePage=1"
+                            },
+                            {
+                              "rel": "dernier",
+                              "href": "http://localhost/api/v1/metiers?ids=MET_356&ids=MET_355&ids=MET_358&numeroDePage=1"
+                            },
+                            {
+                              "rel": "actuel",
+                              "href": "http://localhost/api/v1/metiers?ids=MET_356&ids=MET_355&ids=MET_358&numeroDePage=1"
                             }
                           ]
                         }
@@ -199,6 +228,15 @@ class MetierControllerTest(
                     ),
                 )
             `when`(recupererMetiersService.recupererMetiers(idsMetier)).thenReturn(metiers)
+            val hateoas =
+                Hateoas(
+                    pageActuelle = 1,
+                    pageSuivante = null,
+                    premierePage = 1,
+                    dernierePage = 1,
+                    listeCoupee = metiers,
+                )
+            `when`(hateoasBuilder.creerHateoas(liste = metiers, numeroDePageActuelle = 1, tailleLot = 30)).thenReturn(hateoas)
 
             // When & Then
             mvc.perform(
@@ -245,6 +283,20 @@ class MetierControllerTest(
                                 }
                               ]
                             }
+                          ],
+                          "liens": [
+                            {
+                              "rel": "premier",
+                              "href": "http://localhost/api/v1/metiers?ids=MET_356&ids=MET_355&ids=MET_358&numeroDePage=1"
+                            },
+                            {
+                              "rel": "dernier",
+                              "href": "http://localhost/api/v1/metiers?ids=MET_356&ids=MET_355&ids=MET_358&numeroDePage=1"
+                            },
+                            {
+                              "rel": "actuel",
+                              "href": "http://localhost/api/v1/metiers?ids=MET_356&ids=MET_355&ids=MET_358&numeroDePage=1"
+                            }
                           ]
                         }
                         """.trimIndent(),
@@ -264,6 +316,90 @@ class MetierControllerTest(
         fun `si appel sans ids, doit retourner 400`() {
             // When & Then
             mvc.perform(get("/api/v1/metiers")).andExpect(status().isBadRequest)
+        }
+
+        @ConnecteAvecUnEleve(idEleve = "adcf627c-36dd-4df5-897b-159443a6d49c")
+        @Test
+        fun `si le numéro de page dépasse le nombre de page maximum, doit retourner 400`() {
+            // Given
+            val idsMetier = listOf("MET_356", "MET_355", "MET_358")
+            val metiers =
+                listOf(
+                    Metier(
+                        id = "MET_356",
+                        nom = "ingénieur / ingénieure matériaux",
+                        descriptif =
+                            "<p>L'ingénieur matériaux intervient de la conception à l'utilisation des matériaux. À la pointe " +
+                                "de l'innovation, cet expert met ses compétences au service d'un bureau d'études, d'une entreprise " +
+                                "industrielle ou d'un organisme de recherche.</p>",
+                        liens =
+                            listOf(
+                                Lien(
+                                    nom = "Management et ingénierie études, recherche et développement industriel",
+                                    url = "https://candidat.pole-emploi.fr/marche-du-travail/fichemetierrome?codeRome=H1206",
+                                ),
+                                Lien(
+                                    nom = "ingénieur / ingénieure matériaux",
+                                    url = "https://explorer-avenirs.onisep.fr/http/redirection/metier/slug/MET.356",
+                                ),
+                            ),
+                    ),
+                    Metier(
+                        id = "MET_355",
+                        nom = "responsable de laboratoire de contrôle en chimie",
+                        descriptif =
+                            "<p> Dernière étape avant la mise sur le marché d'un produit issu de l'industrie de la chimie : " +
+                                "son contrôle. Sous la direction du responsable de laboratoire de contrôle, des tests sont effectués " +
+                                "pour évaluer sa qualité et sa conformité aux normes. </p>",
+                        liens = emptyList(),
+                    ),
+                    Metier(
+                        id = "MET_358",
+                        nom = "pilote d'hélicoptère",
+                        descriptif =
+                            "<p>Le pilote d'hélicoptère, militaire ou civil, participe à des missions de combat ou de secours. " +
+                                "Il est capable de se poser n'importe où et d'évoluer en montagne. Sang-froid et ténacité sont " +
+                                "nécessaires pour exercer ce métier.</p>",
+                        liens =
+                            listOf(
+                                Lien(
+                                    nom = "pilote d'hélicoptère",
+                                    url = "https://explorer-avenirs.onisep.fr/http/redirection/metier/slug/MET.358",
+                                ),
+                                Lien(
+                                    nom = "Pilotage et navigation technique aérienne",
+                                    url = "https://candidat.pole-emploi.fr/marche-du-travail/fichemetierrome?codeRome=N2102",
+                                ),
+                            ),
+                    ),
+                )
+            `when`(recupererMetiersService.recupererMetiers(idsMetier)).thenReturn(metiers)
+            val exception =
+                MonProjetSupBadRequestException(
+                    code = "PAGE_DEMANDEE_INXISTANTE",
+                    msg = "La page 10 n'existe pas. Veuillez en donner une entre 1 et 5",
+                )
+            `when`(hateoasBuilder.creerHateoas(liste = metiers, numeroDePageActuelle = 10, tailleLot = 30)).thenThrow(exception)
+
+            // When & Then
+            mvc.perform(
+                get("/api/v1/metiers?ids=MET_356&ids=MET_355&ids=MET_358&numeroDePage=10"),
+            ).andDo(
+                MockMvcResultHandlers.print(),
+            ).andExpect(status().isBadRequest).andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON))
+                .andExpect(
+                    content().json(
+                        """
+                        {
+                          "type": "about:blank",
+                          "title": "PAGE_DEMANDEE_INXISTANTE",
+                          "status": 400,
+                          "detail": "La page 10 n'existe pas. Veuillez en donner une entre 1 et 5",
+                          "instance": "/api/v1/metiers"
+                        }
+                        """.trimIndent(),
+                    ),
+                )
         }
     }
 
@@ -289,6 +425,15 @@ class MetierControllerTest(
                     tailleMinimumRecherche = 2,
                 ),
             ).thenReturn(metiers)
+            val hateoas =
+                Hateoas(
+                    pageActuelle = 1,
+                    pageSuivante = null,
+                    premierePage = 1,
+                    dernierePage = 1,
+                    listeCoupee = metiers,
+                )
+            `when`(hateoasBuilder.creerHateoas(liste = metiers, numeroDePageActuelle = 1, tailleLot = 30)).thenReturn(hateoas)
 
             // When & Then
             mvc.perform(
@@ -327,6 +472,20 @@ class MetierControllerTest(
                               "id": "MET_98",
                               "nom": "sellier/ère"
                             }
+                          ],
+                          "liens": [
+                            {
+                              "rel": "premier",
+                              "href": "http://localhost/api/v1/metiers/recherche/succincte?recherche=cheval&numeroDePage=1"
+                            },
+                            {
+                              "rel": "dernier",
+                              "href": "http://localhost/api/v1/metiers/recherche/succincte?recherche=cheval&numeroDePage=1"
+                            },
+                            {
+                              "rel": "actuel",
+                              "href": "http://localhost/api/v1/metiers/recherche/succincte?recherche=cheval&numeroDePage=1"
+                            }
                           ]
                         }
                         """.trimIndent(),
@@ -354,6 +513,15 @@ class MetierControllerTest(
                     tailleMinimumRecherche = 2,
                 ),
             ).thenReturn(metiers)
+            val hateoas =
+                Hateoas(
+                    pageActuelle = 1,
+                    pageSuivante = null,
+                    premierePage = 1,
+                    dernierePage = 1,
+                    listeCoupee = metiers,
+                )
+            `when`(hateoasBuilder.creerHateoas(liste = metiers, numeroDePageActuelle = 1, tailleLot = 30)).thenReturn(hateoas)
 
             // When & Then
             mvc.perform(
@@ -391,6 +559,20 @@ class MetierControllerTest(
                             {
                               "id": "MET_98",
                               "nom": "sellier/ère"
+                            }
+                          ],
+                          "liens": [
+                            {
+                              "rel": "premier",
+                              "href": "http://localhost/api/v1/metiers/recherche/succincte?recherche=cheval&numeroDePage=1"
+                            },
+                            {
+                              "rel": "dernier",
+                              "href": "http://localhost/api/v1/metiers/recherche/succincte?recherche=cheval&numeroDePage=1"
+                            },
+                            {
+                              "rel": "actuel",
+                              "href": "http://localhost/api/v1/metiers/recherche/succincte?recherche=cheval&numeroDePage=1"
                             }
                           ]
                         }
@@ -448,6 +630,53 @@ class MetierControllerTest(
                           "title": "REQUETE_TROP_LONGUE",
                           "status": 400,
                           "detail": "La taille de la requête dépasse la taille maximale de 50 caractères",
+                          "instance": "/api/v1/metiers/recherche/succincte"
+                        }
+                        """.trimIndent(),
+                    ),
+                )
+        }
+
+        @ConnecteAvecUnEleve(idEleve = "adcf627c-36dd-4df5-897b-159443a6d49c")
+        @Test
+        fun `si le numéro de page dépasse le nombre de page maximum, doit retourner 400`() {
+            // Given
+            val metiers =
+                listOf(
+                    MetierCourt(id = "MET_454", nom = "garde à cheval"),
+                    MetierCourt(id = "MET_471", nom = "entraîneur / entraîneuse de chevaux"),
+                    MetierCourt(id = "MET_682", nom = "maréchal-ferrant / maréchale-ferrante"),
+                    MetierCourt(id = "MET_155", nom = "lad-jockey, lad-driver"),
+                    MetierCourt(id = "MET_345", nom = "moniteur/trice d'activités équestres"),
+                    MetierCourt(id = "MET_19", nom = "palefrenier / palefrenière"),
+                    MetierCourt(id = "MET_98", nom = "sellier/ère"),
+                )
+            `when`(
+                rechercherMetiersService.rechercherMetiers(
+                    recherche = "cheval",
+                    tailleMinimumRecherche = 2,
+                ),
+            ).thenReturn(metiers)
+            val exception =
+                MonProjetSupBadRequestException(
+                    code = "PAGE_DEMANDEE_INXISTANTE",
+                    msg = "La page 32 n'existe pas. Veuillez en donner une entre 1 et 6",
+                )
+            `when`(hateoasBuilder.creerHateoas(liste = metiers, numeroDePageActuelle = 32, tailleLot = 30)).thenThrow(exception)
+
+            // When & Then
+            mvc.perform(
+                get("/api/v1/metiers/recherche/succincte?recherche=cheval&numeroDePage=32"),
+            ).andDo(MockMvcResultHandlers.print()).andExpect(status().isBadRequest)
+                .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON))
+                .andExpect(
+                    content().json(
+                        """
+                        {
+                          "type": "about:blank",
+                          "title": "PAGE_DEMANDEE_INXISTANTE",
+                          "status": 400,
+                          "detail": "La page 32 n'existe pas. Veuillez en donner une entre 1 et 6",
                           "instance": "/api/v1/metiers/recherche/succincte"
                         }
                         """.trimIndent(),
