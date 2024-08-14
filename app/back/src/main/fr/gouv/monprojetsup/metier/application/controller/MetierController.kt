94dba610fb6a99e7cb698973d5c339425ce33709
Aurelia Del Zotto <aurelia.del.zotto@octo.com>
2024-08-14 14:40:04 +0200

    feat(166) : Ajout de la pagination sur les endpoints métiers

--- app/back/src/main/fr/gouv/monprojetsup/metier/application/controller/MetierController.kt
+++ app/back/src/main/fr/gouv/monprojetsup/metier/application/controller/MetierController.kt
@@ -1,12 +1,15 @@
 package fr.gouv.monprojetsup.metier.application.controller
 
 import fr.gouv.monprojetsup.commun.erreur.domain.MonProjetSupBadRequestException
+import fr.gouv.monprojetsup.commun.hateoas.domain.PaginationConstants.PARAMETRE_NUMERO_PAGE
+import fr.gouv.monprojetsup.commun.hateoas.usecase.HateoasBuilder
 import fr.gouv.monprojetsup.metier.application.dto.MetierCourtDTO
 import fr.gouv.monprojetsup.metier.application.dto.MetierDTO
 import fr.gouv.monprojetsup.metier.application.dto.MetiersCourtsDTO
 import fr.gouv.monprojetsup.metier.application.dto.MetiersDTO
 import fr.gouv.monprojetsup.metier.usecase.RechercherMetiersService
 import fr.gouv.monprojetsup.metier.usecase.RecupererMetiersService
+import io.swagger.v3.oas.annotations.Parameter
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -17,10 +20,12 @@ import org.springframework.web.bind.annotation.RestController
 class MetierController(
     val rechercherMetiersService: RechercherMetiersService,
     val recupererMetiersService: RecupererMetiersService,
+    val hateoasBuilder: HateoasBuilder,
 ) {
     @GetMapping("/recherche/succincte")
     fun getRechercheMetierSuccincte(
         @RequestParam recherche: String,
+        @Parameter(description = "Numéro de page") @RequestParam(defaultValue = "1", value = PARAMETRE_NUMERO_PAGE) numeroDePage: Int,
     ): MetiersCourtsDTO {
         if (recherche.length > TAILLE_MAXIMAL_RECHERCHE) {
             throw MonProjetSupBadRequestException(
@@ -38,21 +43,27 @@ class MetierController(
                 recherche = recherche,
                 tailleMinimumRecherche = TAILLE_MINIMUM_RECHERCHE,
             )
-        return MetiersCourtsDTO(
-            metiers = metiers.map { MetierCourtDTO(it) },
-        )
+        val hateoas = hateoasBuilder.creerHateoas(metiers, numeroDePage, TAILLE_LOT_RECHERCHE_SUCCINCTE)
+        val dto = MetiersCourtsDTO(metiers = metiers.map { MetierCourtDTO(it) })
+        dto.ajouterHateoas(hateoas)
+        return dto
     }
 
     @GetMapping
     fun getMetiers(
         @RequestParam ids: List<String>,
+        @Parameter(description = "Numéro de page") @RequestParam(defaultValue = "1", value = PARAMETRE_NUMERO_PAGE) numeroDePage: Int,
     ): MetiersDTO {
         val metiers = recupererMetiersService.recupererMetiers(ids)
-        return MetiersDTO(metiers.map { MetierDTO(it) })
+        val hateoas = hateoasBuilder.creerHateoas(metiers, numeroDePage, TAILLE_LOT_ID)
+        val dto = MetiersDTO(metiers.map { MetierDTO(it) })
+        dto.ajouterHateoas(hateoas)
+        return dto
     }
 
     companion object {
-        private const val NOMBRE_METIERS_MAXIMAL_RETOURNES = 30
+        private const val TAILLE_LOT_RECHERCHE_SUCCINCTE = 30
+        private const val TAILLE_LOT_ID = 30
         private const val TAILLE_MINIMUM_RECHERCHE = 2
         private const val TAILLE_MAXIMAL_RECHERCHE = 50
     }
--- app/back/src/main/fr/gouv/monprojetsup/metier/application/dto/MetiersDTO.kt
+++ app/back/src/main/fr/gouv/monprojetsup/metier/application/dto/MetiersDTO.kt
@@ -1,5 +1,7 @@
 package fr.gouv.monprojetsup.metier.application.dto
 
+import fr.gouv.monprojetsup.commun.hateoas.application.dto.PaginationDTO
+
 data class MetiersDTO(
     val metiers: List<MetierDTO>,
-)
+) : PaginationDTO()
