144af93e5ef2a6c905c38ff4b076111f354ff166
Hugo Gimbert <Numero7@users.noreply.github.com>
2024-08-14 08:56:59 +0200

    etl op v1

---
+++ v1/back/java/etl/etl-updatedb/src/test/kotlin/fr/gouv/monprojetsup/data/etl/BDDRepositoryTest.kt
@@ -0,0 +1,44 @@
+package fr.gouv.monprojetsup.data.etl
+
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = NONE)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
+class BDDRepositoryTest {
+    companion object {
+        @JvmStatic
+        private var postgres: PostgreSQLContainer<*> = PostgreSQLContainer("postgres:15-alpine")
+
+        @JvmStatic
+        @BeforeAll
+        fun beforeAll() {
+            postgres.start()
+        }
+
+        @JvmStatic
+        @AfterAll
+        fun afterAll() {
+            postgres.stop()
+        }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun configureProperties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", postgres::getJdbcUrl)
+            registry.add("spring.datasource.username", postgres::getUsername)
+            registry.add("spring.datasource.password", postgres::getPassword)
+            registry.add("flyway.url", postgres::getJdbcUrl)
+            registry.add("flyway.user", postgres::getUsername)
+            registry.add("flyway.password", postgres::getPassword)
+        }
+    }
+}
