4a2c26f89e9baebfe01a19f182c6a5d8ba0be314
Hugo Gimbert <hugo.gimbert@cnrs.fr>
2024-09-01 08:10:30 +0200

    added missing migration file

---
+++ app/back/src/main/resources/db/migration/V1_13__adaptation_profil.sql
@@ -0,0 +1,4 @@
+
+ALTER TABLE profil_eleve DROP CONSTRAINT profil_eleve_id_baccalaureat_fkey;
+
+ALTER TABLE profil_eleve ADD CONSTRAINT profil_eleve_id_ref_baccalaureat_fkey FOREIGN KEY (id_baccalaureat) REFERENCES ref_baccalaureat(id);
