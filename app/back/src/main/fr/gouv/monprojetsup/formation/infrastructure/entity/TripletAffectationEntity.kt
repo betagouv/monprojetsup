4ad782439c29404e9801a5b7550bd4ad27938847
Aurelia Del Zotto <aurelia.del.zotto@octo.com>
2024-09-04 11:53:38 +0200

    feat(242) : Supprimer coordonnées géographique de la table ref_triplet_affectation

--- app/back/src/main/fr/gouv/monprojetsup/formation/infrastructure/entity/TripletAffectationEntity.kt
+++ app/back/src/main/fr/gouv/monprojetsup/formation/infrastructure/entity/TripletAffectationEntity.kt
@@ -2,12 +2,10 @@ package fr.gouv.monprojetsup.formation.infrastructure.entity
 
 import fr.gouv.monprojetsup.eleve.domain.entity.Commune
 import fr.gouv.monprojetsup.formation.domain.entity.TripletAffectation
-import io.hypersistence.utils.hibernate.type.array.ListArrayType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
-import org.hibernate.annotations.Type
 
 @Entity
 @Table(name = "ref_triplet_affectation")
@@ -24,9 +22,11 @@ class TripletAffectationEntity {
     @Column(name = "code_commune", nullable = false)
     lateinit var codeCommune: String
 
-    @Type(ListArrayType::class)
-    @Column(name = "coordonnees_geographiques", nullable = false)
-    lateinit var coordonneesGeographiques: List<Double>
+    @Column(name = "latitude", nullable = true)
+    var latitude: Double? = null
+
+    @Column(name = "longitude", nullable = true)
+    var longitude: Double? = null
 
     @Column(name = "id_formation", nullable = false)
     lateinit var idFormation: String
@@ -39,8 +39,8 @@ class TripletAffectationEntity {
                 Commune(
                     codeInsee = codeCommune,
                     nom = commune,
-                    latitude = coordonneesGeographiques[0],
-                    longitude = coordonneesGeographiques[1],
+                    latitude = latitude ?: 0.0,
+                    longitude = longitude ?: 0.0,
                 ),
         )
 }
