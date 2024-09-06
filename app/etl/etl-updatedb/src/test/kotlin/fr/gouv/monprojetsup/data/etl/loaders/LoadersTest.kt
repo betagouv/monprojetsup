a3e69eb4e0fa6678e22b84a4b2539bfdb2cc3d05
Hugo Gimbert <hugo.gimbert@cnrs.fr>
2024-09-06 18:49:48 +0200

    removed je veux from labels

--- app/etl/etl-updatedb/src/test/kotlin/fr/gouv/monprojetsup/data/etl/loaders/LoadersTest.kt
+++ app/etl/etl-updatedb/src/test/kotlin/fr/gouv/monprojetsup/data/etl/loaders/LoadersTest.kt
@@ -2,9 +2,15 @@ package fr.gouv.monprojetsup.data.etl.loaders
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 
 
-class LoadersTest() {
+@SpringBootTest(classes = [LoadersTest::class, DataSources::class] )
+class LoadersTest {
+
+    @Autowired
+    lateinit var sources : DataSources
 
     @Test
     fun `Doit réussir à récupérer les formaitons ideo depuis le site onisep`() {
@@ -43,12 +49,20 @@ class LoadersTest() {
     }
 
     @Test
-    fun `Toutes les fiches métiers ideo ont un descriptif `() {
+    fun `Toutes les fiches métiers ideo ont un descriptif`() {
         // Given
         val metiers = OnisepDataLoader.loadFichesMetiersIDeo()
         // When
         // Then
         assertThat(metiers.map{ m -> m.descriptif}).allMatch { it.isNotBlank() }
+    }
+
+
+    @Test fun `Doit réussir à charger les intérêts, qui ne contiennent pas le terme je veux`() {
+        val interets = OnisepDataLoader.loadInterets(sources)
+        val labels = interets.groupeInterets.flatMap { it.items }.map { it.frontLabel}
+        assertThat(labels).isNotEmpty
+        assertThat(labels).allMatch { !it.contains("je veux") }
     }
 
 
