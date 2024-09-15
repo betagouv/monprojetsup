c212ef1c46d4d9cca915c18954f05a4feb2fadbc
Hugo Gimbert <hugo.gimbert@cnrs.fr>
2024-09-15 08:29:32 +0200

    capitalize first letter of categories

--- app/etl/etl-common/src/main/java/fr/gouv/monprojetsup/data/model/taxonomie/Taxonomie.java
+++ app/etl/etl-common/src/main/java/fr/gouv/monprojetsup/data/model/taxonomie/Taxonomie.java
@@ -1,12 +1,13 @@
 package fr.gouv.monprojetsup.data.model.taxonomie;
 
+import fr.gouv.monprojetsup.data.Constants;
 import lombok.val;
 import org.apache.commons.lang3.tuple.Pair;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.logging.Logger;
 import java.util.stream.Stream;
 
 import static fr.gouv.monprojetsup.data.Constants.includeKey;
@@ -28,9 +29,7 @@ public record Taxonomie(
         val result = new HashMap<String, String>();
         this.categories.forEach(
                 g -> g.elements().forEach(
-                        item -> {
-                            result.put(item.getId(), includeKeys ? includeKey(item.getId(), item.label()) : item.label());
-                        }
+                        item -> result.put(item.getId(), includeKeys ? includeKey(item.getId(), item.label()) : item.label())
                 )
         );
         return result;
@@ -44,4 +43,66 @@ public record Taxonomie(
                         )
                 );
     }
+
+
+    public record TaxonomieCategorie(
+            @NotNull String label,
+            @NotNull String emoji,
+            List<@NotNull TaxonomieElement> elements
+    ) {
+        private static final Logger LOGGER = Logger.getLogger(TaxonomieCategorie.class.getSimpleName());
+
+        public TaxonomieCategorie(String categorieLabel, String emoji) {
+            this(capitalizeFirstLetter(categorieLabel), emoji, new ArrayList<>());
+        }
+
+        @NotNull
+        public String getId() {
+            return Constants.cleanup(label);
+        }
+
+
+        public void retainAll(Set<String> interetsUsed) {
+            if (interetsUsed.contains(getId())) return;
+            List<@NotNull TaxonomieElement> toRemove =
+                    elements.stream()
+                            .filter(it -> it.atomes.keySet().stream().noneMatch(k -> interetsUsed.contains(Constants.cleanup(k))))
+                            .toList();
+            toRemove.forEach(item -> LOGGER.info("Intérêt non utilisé: " + item));
+            elements.removeAll(toRemove);
+        }
+
+        public record TaxonomieElement(
+                @NotNull Map<String,@NotNull String> atomes,
+                @NotNull String label,
+                @NotNull String emoji,
+                @Nullable String description
+
+
+        ) {
+
+            public String getId() {
+                return Constants.cleanup(label).trim();
+            }
+
+            public static TaxonomieElement build(
+                    @NotNull Map<String,@NotNull String> atomes,
+                    @NotNull String label,
+                    @NotNull String emoji,
+                    @Nullable  String description) {
+                return new TaxonomieElement(atomes, capitalizeFirstLetter(label), emoji, description);
+            }
+        }
+    }
+
+    public static @NotNull String capitalizeFirstLetter(@NotNull String sentence) {
+        // Trim leading and trailing spaces
+        sentence = sentence.trim();
+        if (sentence.isEmpty()) {
+            return sentence;
+        }
+        // Capitalize the first character and concatenate it with the rest of the string
+        return sentence.substring(0, 1).toUpperCase() + sentence.substring(1);
+    }
+
 }
