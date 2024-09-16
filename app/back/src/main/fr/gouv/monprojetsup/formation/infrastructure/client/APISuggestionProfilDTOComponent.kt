feb53c7cb19e35a813f12ce63c59388894e20ea2
Hugo Gimbert <hugo.gimbert@cnrs.fr>
2024-09-16 23:32:09 +0200

    passe les interets et specialite stels quels a l'api suggestion

--- app/back/src/main/fr/gouv/monprojetsup/formation/infrastructure/client/APISuggestionProfilDTOComponent.kt
+++ app/back/src/main/fr/gouv/monprojetsup/formation/infrastructure/client/APISuggestionProfilDTOComponent.kt
@@ -2,24 +2,13 @@ package fr.gouv.monprojetsup.formation.infrastructure.client
 
 import fr.gouv.monprojetsup.authentification.domain.entity.ProfilEleve
 import fr.gouv.monprojetsup.formation.infrastructure.dto.APISuggestionProfilDTO
-import fr.gouv.monprojetsup.referentiel.domain.port.InteretRepository
-import fr.gouv.monprojetsup.referentiel.domain.port.SpecialitesRepository
 import org.springframework.stereotype.Component
 
 @Component
-class APISuggestionProfilDTOComponent(
-    private val specialitesRepository: SpecialitesRepository,
-    private val interetRepository: InteretRepository,
-) {
+class APISuggestionProfilDTOComponent {
     fun creerAPISuggestionProfilDTO(profilEleve: ProfilEleve.Identifie): APISuggestionProfilDTO {
-        val specialites =
-            profilEleve.specialites?.takeUnless { it.isEmpty() }?.let {
-                specialitesRepository.recupererLesSpecialites(it).map { specialite -> specialite.label }
-            }
-        val centresInterets =
-            profilEleve.centresInterets?.takeUnless { it.isEmpty() }?.let { centresInterets ->
-                interetRepository.recupererLesInteretsDeSousCategories(centresInterets).map { interet -> interet.id }
-            }
+        val specialites = profilEleve.specialites?.takeUnless { it.isEmpty() }
+        val centresInterets = profilEleve.centresInterets?.takeUnless { it.isEmpty() }
         val apiSuggestionProfilDTO =
             APISuggestionProfilDTO(
                 profilEleve = profilEleve,
