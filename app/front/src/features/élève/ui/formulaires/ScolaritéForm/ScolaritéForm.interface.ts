7c8fe7f48073dcdbe05d5363c0cef8c4eb643846
Maxime <maxime.vigier@octo.com>
2024-09-20 14:14:58 +0200

    refactor: spécialités dans scolarité form (47)

--- app/front/src/features/élève/ui/formulaires/ScolaritéForm/ScolaritéForm.interface.ts
+++ app/front/src/features/élève/ui/formulaires/ScolaritéForm/ScolaritéForm.interface.ts
@@ -1,4 +1,10 @@
-import { type Bac, type ClasseÉlève } from "@/features/référentielDonnées/domain/référentielDonnées.interface";
+import { type Élève } from "@/features/élève/domain/élève.interface";
+import {
+  type Bac,
+  type ClasseÉlève,
+  type RéférentielDonnées,
+} from "@/features/référentielDonnées/domain/référentielDonnées.interface";
+import { type UseFormGetValues, type UseFormSetValue } from "react-hook-form";
 
 export type ScolaritéFormProps = {
   formId: string;
@@ -17,4 +23,11 @@ export type BacOptions = Array<{
 
 export type useScolaritéFormArgs = {
   àLaSoumissionDuFormulaireAvecSuccès?: () => void;
+};
+
+export type UseSpécialitésFormArgs = {
+  référentielDonnées?: RéférentielDonnées | null;
+  valeurBac: Élève["bac"] | null;
+  setValue: UseFormSetValue<Élève>;
+  getValues: UseFormGetValues<Élève>;
 };
