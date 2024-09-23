b39ad18fa5c84b4a45ed73e0ff79043f9ca0bb32
Maxime <maxime.vigier@octo.com>
2024-09-23 17:34:47 +0200

    fix: correction de la redirection après login (231)

--- app/front/src/configuration/lib/oidc-client.ts
+++ app/front/src/configuration/lib/oidc-client.ts
@@ -5,7 +5,7 @@ export const userManagerOIDCClient = new UserManager({
   authority: env.VITE_KEYCLOAK_ROYAUME_URL,
   client_id: env.VITE_KEYCLOAK_CLIENT_ID,
   client_secret: env.VITE_KEYCLOAK_CLIENT_SECRET, // Oui je sais c'est bizarre de mettre un secret côté front mais c'est la configuration Avenir(s) :)
-  redirect_uri: env.VITE_APP_URL,
+  redirect_uri: window.location.href,
   post_logout_redirect_uri: env.VITE_PUBLIC_WEBSITE_URL,
   userStore: new WebStorageStateStore({ store: window.sessionStorage }),
   monitorSession: true,
