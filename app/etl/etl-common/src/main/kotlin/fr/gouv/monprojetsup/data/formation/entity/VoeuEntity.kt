85b053be71595c3a0298146664af251c155a3129
Hugo Gimbert <hugo.gimbert@cnrs.fr>
2024-09-29 01:51:22 +0200

    fix nomenclature table

--- app/etl/etl-common/src/main/kotlin/fr/gouv/monprojetsup/data/formation/entity/VoeuEntity.kt
+++ app/etl/etl-common/src/main/kotlin/fr/gouv/monprojetsup/data/formation/entity/VoeuEntity.kt
@@ -12,7 +12,7 @@ import org.hibernate.annotations.JdbcTypeCode
 import org.hibernate.type.SqlTypes
 
 @Entity
-@Table(name = "ref_voeux")
+@Table(name = "ref_voeu")
 class VoeuEntity {
     constructor()
     constructor(voeu: Voeu) {
