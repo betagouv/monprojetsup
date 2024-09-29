4ab61879472e57dcc76e4ba588f1e5fcdd5eb7aa
Hugo Gimbert <hugo.gimbert@cnrs.fr>
2024-09-30 01:08:08 +0200

    updates experts accounts script

--- keycloak/scripts/create_experts_accounts.py
+++ keycloak/scripts/create_experts_accounts.py
@@ -1,7 +1,22 @@
 import requests
 import os
+import csv
 
 from dotenv import load_dotenv
+
+# charger le csv
+experts = []
+with open("experts.csv", mode="r") as file:
+    csv_reader = csv.DictReader(file)  # Utiliser DictReader pour lire avec les en-têtes
+    for row in csv_reader:
+        experts.append(
+            [
+                row["EMAIL"],
+                row["NOM"],
+                row["PRENOM"],
+            ]
+        )
+
 
 # Load environment variables from .env file
 load_dotenv()
@@ -12,6 +27,7 @@ realm_name = os.getenv("REALM_NAME")
 client_id = os.getenv("CLIENT_ID")
 admin_username = os.getenv("ADMIN_USERNAME")
 admin_password = os.getenv("ADMIN_PASSWORD")
+experts_password = os.getenv("EXPERTS_PASSWORD")
 
 # Obtenir le token d'accès
 token_url = f"{keycloak_url}/realms/{realm_name}/protocol/openid-connect/token"
@@ -34,26 +50,44 @@ else:
     print(f"Failed to obtain token: {response.text}")
     exit(1)
 
+
 # Créer un nouvel utilisateur
 create_user_url = f"{keycloak_url}/admin/realms/{realm_name}/users"
-user_data = {
-    "username": "newuser",  # Nom d'utilisateur
-    "email": "newuser@example.com",  # Email de l'utilisateur
-    "firstName": "New",
-    "lastName": "User",
-    "enabled": True,  # Activer le compte
-}
+for expert in experts:
+    email = expert[0]
+    nom = expert[1]
+    prenom = expert[2]
 
-# Ajouter le token dans les headers pour l'authentification
-create_user_headers = {
-    "Authorization": f"Bearer {access_token}",
-    "Content-Type": "application/json",
-}
+    for idx in range(1, 11):
+        print(f"Processing {email}_{idx}")
+        user_data = {
+            "username": f"{email}_{idx}",  # Nom d'utilisateur
+            "email": f"{email}",  # Email de l'utilisateur
+            "firstName": f"{prenom}",
+            "lastName": f"{nom}",
+            "enabled": True,  # Activer le compte
+            "emailVerified": True,
+            "attributes": {"profile": "expert"},
+            "credentials": [
+                {
+                    "type": "password",
+                    "value": f"{experts_password}",
+                    "temporary": False,
+                }
+            ],
+        }
 
-# Envoyer la requête pour créer l'utilisateur
-create_user_response = requests.post(create_user_url, json=user_data, headers=create_user_headers)
-if create_user_response.status_code == 201:
-    print("User created successfully!")
-else:
-    print(f"Failed to create user: {create_user_response.text}")
-    
+        # Ajouter le token dans les headers pour l'authentification
+        create_user_headers = {
+            "Authorization": f"Bearer {access_token}",
+            "Content-Type": "application/json",
+        }
+
+        # Envoyer la requête pour créer l'utilisateur
+        create_user_response = requests.post(
+            create_user_url, json=user_data, headers=create_user_headers
+        )
+        if create_user_response.status_code == 201:
+            print(f"User {email}_{idx} created successfully!")
+        else:
+            print(f"Failed to create user: {create_user_response.text}")
