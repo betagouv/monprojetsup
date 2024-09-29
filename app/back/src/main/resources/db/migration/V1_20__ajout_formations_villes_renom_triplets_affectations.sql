ba1d9290a6b6faa5016da119028e624d9db23e25
Hugo Gimbert <hugo.gimbert@cnrs.fr>
2024-09-29 02:12:48 +0200

    fix + rename clum in ref_formatioj

--- app/back/src/main/resources/db/migration/V1_20__ajout_formations_villes_renom_triplets_affectations.sql
+++ app/back/src/main/resources/db/migration/V1_20__ajout_formations_villes_renom_triplets_affectations.sql
@@ -1,7 +1,9 @@
 ALTER TABLE ref_triplet_affectation RENAME TO ref_voeu;
 
-CREATE TABLE ref_ville_voeu (
+CREATE TABLE ref_join_ville_voeu (
     id_ville varchar(255) NOT NULL,
     voeux_10km character varying[] NOT NULL,
     voeux_30km character varying[] NOT NULL
 );
+
+ALTER TABLE ref_formation RENAME COLUMN formations_associees to formations_psup;
