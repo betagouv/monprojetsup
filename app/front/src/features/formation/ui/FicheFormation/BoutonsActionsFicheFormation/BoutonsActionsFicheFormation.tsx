02cff7678d6ef88bf2cda052059d97cae01065cb
Maxime <maxime.vigier@octo.com>
2024-10-04 16:51:16 +0200

    fix: masque l'encart voeux pour merge (220)

--- app/front/src/features/formation/ui/FicheFormation/BoutonsActionsFicheFormation/BoutonsActionsFicheFormation.tsx
+++ app/front/src/features/formation/ui/FicheFormation/BoutonsActionsFicheFormation/BoutonsActionsFicheFormation.tsx
@@ -2,7 +2,6 @@ import { type BoutonsActionsFicheFormationProps } from "./BoutonsActionsFicheFor
 import useBoutonsActionsFicheFormation from "./useBoutonsActionsFicheFormation";
 import Bouton from "@/components/Bouton/Bouton";
 import { i18n } from "@/configuration/i18n/i18n";
-import VoeuxFicheFormation from "@/features/formation/ui/FicheFormation/Voeux/Voeux";
 
 const BoutonsActionsFicheFormation = ({ formation }: BoutonsActionsFicheFormationProps) => {
   const TAILLE_BOUTON = "grand";
@@ -18,58 +17,55 @@ const BoutonsActionsFicheFormation = ({ formation }: BoutonsActionsFicheFormatio
   });
 
   return (
-    <>
-      <div className="grid justify-start justify-items-start gap-4 sm:grid-flow-col">
-        {!estMasquée && !estFavorite && (
-          <Bouton
-            auClic={ajouterEnFavori}
-            icône={{ position: "gauche", classe: "fr-icon-heart-line" }}
-            label={i18n.COMMUN.AJOUTER_À_MA_SÉLECTION}
-            taille={TAILLE_BOUTON}
-            type="button"
-          />
-        )}
-        {estFavorite && (
-          <>
-            <div className="grid grid-flow-col items-center gap-2 font-medium text-[--artwork-minor-red-marianne]">
-              <span
-                aria-hidden="true"
-                className="fr-icon-heart-fill"
-              />
-              {i18n.COMMUN.AJOUTÉ_À_MA_SÉLECTION}
-            </div>
-            <Bouton
-              auClic={supprimerDesFavoris}
-              icône={{ position: "gauche", classe: "fr-icon-close-line" }}
-              label={i18n.COMMUN.SUPPRIMER_DE_MA_SÉLECTION}
-              taille={TAILLE_BOUTON}
-              type="button"
-              variante="secondaire"
+    <div className="grid justify-start justify-items-start gap-4 sm:grid-flow-col">
+      {!estMasquée && !estFavorite && (
+        <Bouton
+          auClic={ajouterEnFavori}
+          icône={{ position: "gauche", classe: "fr-icon-heart-line" }}
+          label={i18n.COMMUN.AJOUTER_À_MA_SÉLECTION}
+          taille={TAILLE_BOUTON}
+          type="button"
+        />
+      )}
+      {estFavorite && (
+        <>
+          <div className="grid grid-flow-col items-center gap-2 font-medium text-[--artwork-minor-red-marianne]">
+            <span
+              aria-hidden="true"
+              className="fr-icon-heart-fill"
             />
-          </>
-        )}
-        {!estFavorite && !estMasquée && (
+            {i18n.COMMUN.AJOUTÉ_À_MA_SÉLECTION}
+          </div>
           <Bouton
-            auClic={masquerUneFormation}
-            icône={{ position: "gauche", classe: "fr-icon-eye-off-line" }}
-            label={i18n.COMMUN.NE_PLUS_VOIR}
+            auClic={supprimerDesFavoris}
+            icône={{ position: "gauche", classe: "fr-icon-close-line" }}
+            label={i18n.COMMUN.SUPPRIMER_DE_MA_SÉLECTION}
             taille={TAILLE_BOUTON}
             type="button"
             variante="secondaire"
           />
-        )}
-        {!estFavorite && estMasquée && (
-          <Bouton
-            auClic={afficherÀNouveauUneFormation}
-            icône={{ position: "gauche", classe: "fr-icon-eye-line" }}
-            label={i18n.COMMUN.AFFICHER_À_NOUVEAU}
-            taille={TAILLE_BOUTON}
-            type="button"
-          />
-        )}
-      </div>
-      {estFavorite && <VoeuxFicheFormation formation={formation} />}
-    </>
+        </>
+      )}
+      {!estFavorite && !estMasquée && (
+        <Bouton
+          auClic={masquerUneFormation}
+          icône={{ position: "gauche", classe: "fr-icon-eye-off-line" }}
+          label={i18n.COMMUN.NE_PLUS_VOIR}
+          taille={TAILLE_BOUTON}
+          type="button"
+          variante="secondaire"
+        />
+      )}
+      {!estFavorite && estMasquée && (
+        <Bouton
+          auClic={afficherÀNouveauUneFormation}
+          icône={{ position: "gauche", classe: "fr-icon-eye-line" }}
+          label={i18n.COMMUN.AFFICHER_À_NOUVEAU}
+          taille={TAILLE_BOUTON}
+          type="button"
+        />
+      )}
+    </div>
   );
 };
 
