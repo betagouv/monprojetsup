c7623bab05cfe255bf6a26fd412ecf1db708fa78
aurelia.del.zotto <aurelia.del.zotto@octo.com>
2024-10-08 17:12:31 +0200

    feat(447) : Modifier message tableau de bord

--- app/front/src/features/élève/ui/tableauDeBord/TableauDeBordÉlève/useTableauDeBordÉlève.tsx
+++ app/front/src/features/élève/ui/tableauDeBord/TableauDeBordÉlève/useTableauDeBordÉlève.tsx
@@ -2,15 +2,9 @@ import explorerSVG from "@/assets/explorer.svg";
 import favorisSVG from "@/assets/favoris.svg";
 import profilSVG from "@/assets/profil.svg";
 import { i18n } from "@/configuration/i18n/i18n";
-import { élèveQueryOptions } from "@/features/élève/ui/élèveQueries";
 import { type CarteTableauDeBordÉlèveProps } from "@/features/élève/ui/tableauDeBord/CarteTableauDeBordÉlève/CarteTableauDeBordÉlève.interface";
-import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
-import { useMemo } from "react";
 
 export default function useTableauDeBordÉlève() {
-  useSuspenseQuery(élèveQueryOptions);
-  const { data: élève } = useQuery(élèveQueryOptions);
-
   const cartes: CarteTableauDeBordÉlèveProps[] = [
     {
       titre: i18n.ÉLÈVE.TABLEAU_DE_BORD.CARTES.SUGGESTIONS.TITRE,
@@ -32,23 +26,11 @@ export default function useTableauDeBordÉlève() {
     },
   ];
 
-  const témoignage = useMemo(() => {
-    let contenu: string;
-
-    if (élève?.classe === "terminale") {
-      contenu = i18n.ÉLÈVE.TABLEAU_DE_BORD.TÉMOIGNAGE.TERMINALE;
-    } else if (élève?.classe === "premiere") {
-      contenu = i18n.ÉLÈVE.TABLEAU_DE_BORD.TÉMOIGNAGE.PREMIÈRE;
-    } else {
-      contenu = i18n.ÉLÈVE.TABLEAU_DE_BORD.TÉMOIGNAGE.SECONDE;
-    }
-
-    return {
-      contenu,
-      auteur: i18n.ÉLÈVE.TABLEAU_DE_BORD.TÉMOIGNAGE.AUTEUR,
-      rôle: i18n.ÉLÈVE.TABLEAU_DE_BORD.TÉMOIGNAGE.RÔLE,
-    };
-  }, [élève]);
+  const témoignage = {
+    contenu: i18n.ÉLÈVE.TABLEAU_DE_BORD.TÉMOIGNAGE.PHRASE,
+    auteur: i18n.ÉLÈVE.TABLEAU_DE_BORD.TÉMOIGNAGE.AUTEUR,
+    rôle: i18n.ÉLÈVE.TABLEAU_DE_BORD.TÉMOIGNAGE.RÔLE,
+  };
 
   return {
     témoignage,
