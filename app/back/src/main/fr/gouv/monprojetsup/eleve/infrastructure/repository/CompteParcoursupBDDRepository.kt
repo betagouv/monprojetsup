d88ea07bd3cb294ed8eb08f0316d04d963ad5166
aurelia.del.zotto <aurelia.del.zotto@octo.com>
2024-10-08 16:16:31 +0200

    fix : Retirer log warning compte parcoursup

--- app/back/src/main/fr/gouv/monprojetsup/eleve/infrastructure/repository/CompteParcoursupBDDRepository.kt
+++ app/back/src/main/fr/gouv/monprojetsup/eleve/infrastructure/repository/CompteParcoursupBDDRepository.kt
@@ -1,22 +1,17 @@
 package fr.gouv.monprojetsup.eleve.infrastructure.repository
 
 import fr.gouv.monprojetsup.eleve.domain.port.CompteParcoursupRepository
-import org.slf4j.Logger
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 import java.util.UUID
-import kotlin.jvm.optionals.getOrElse
+import kotlin.jvm.optionals.getOrNull
 
 @Repository
 class CompteParcoursupBDDRepository(
     private val compteParcoursupJPARepository: CompteParcoursupJPARepository,
-    private val logger: Logger,
 ) : CompteParcoursupRepository {
     @Transactional(readOnly = false)
     override fun recupererIdCompteParcoursup(idEleve: UUID): Int? {
-        return compteParcoursupJPARepository.findById(idEleve).getOrElse {
-            logger.warn("L'élève $idEleve n'a pas relié son compte Parcoursup à son compte MonProjetSup")
-            null
-        }?.idParcoursup
+        return compteParcoursupJPARepository.findById(idEleve).getOrNull()?.idParcoursup
     }
 }
--- app/back/src/test/fr/gouv/monprojetsup/eleve/infrastructure/repository/CompteParcoursupBDDRepositoryTest.kt
+++ app/back/src/test/fr/gouv/monprojetsup/eleve/infrastructure/repository/CompteParcoursupBDDRepositoryTest.kt
@@ -5,9 +5,6 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import org.mockito.BDDMockito.then
-import org.mockito.Mock
-import org.slf4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.jdbc.Sql
 import java.util.UUID
@@ -16,14 +13,11 @@ class CompteParcoursupBDDRepositoryTest : BDDRepositoryTest() {
     @Autowired
     lateinit var compteParcoursupJPARepository: CompteParcoursupJPARepository
 
-    @Mock
-    lateinit var logger: Logger
-
     lateinit var compteParcoursupBDDRepository: CompteParcoursupBDDRepository
 
     @BeforeEach
     fun setup() {
-        compteParcoursupBDDRepository = CompteParcoursupBDDRepository(compteParcoursupJPARepository, logger)
+        compteParcoursupBDDRepository = CompteParcoursupBDDRepository(compteParcoursupJPARepository)
     }
 
     @Nested
@@ -43,7 +37,7 @@ class CompteParcoursupBDDRepositoryTest : BDDRepositoryTest() {
 
         @Test
         @Sql("classpath:comptes_parcoursup.sql")
-        fun `Si le compte n'est pas relié, doit logguer un warning et retourner null`() {
+        fun `Si le compte n'est pas relié, doit retourner null`() {
             // Given
             val idEleve = UUID.fromString("129f6d9c-0f6f-4fa4-8107-75b7cb129889")
 
@@ -52,15 +46,11 @@ class CompteParcoursupBDDRepositoryTest : BDDRepositoryTest() {
 
             // Then
             assertThat(result).isNull()
-            then(logger).should().warn(
-                "L'élève 129f6d9c-0f6f-4fa4-8107-75b7cb129889 n'a pas relié " +
-                    "son compte Parcoursup à son compte MonProjetSup",
-            )
         }
 
         @Test
         @Sql("classpath:comptes_parcoursup.sql")
-        fun `Si le compte n'existe pas, doit logguer un warning et retourner null`() {
+        fun `Si le compte n'existe pas, doit retourner null`() {
             // Given
             val idEleve = UUID.fromString("de14aa82-13dc-4eed-82ae-77d038e271e2")
 
@@ -69,10 +59,6 @@ class CompteParcoursupBDDRepositoryTest : BDDRepositoryTest() {
 
             // Then
             assertThat(result).isNull()
-            then(logger).should().warn(
-                "L'élève de14aa82-13dc-4eed-82ae-77d038e271e2 n'a pas relié " +
-                    "son compte Parcoursup à son compte MonProjetSup",
-            )
         }
     }
 }
