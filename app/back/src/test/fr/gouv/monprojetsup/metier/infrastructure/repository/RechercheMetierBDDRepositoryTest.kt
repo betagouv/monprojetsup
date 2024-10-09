a528940474efe2ee311305f03ccf1506aff23cd6
aurelia.del.zotto <aurelia.del.zotto@octo.com>
2024-10-09 19:21:56 +0200

    feat(162) : Les recherches métiers supportent les typos

--- app/back/src/test/fr/gouv/monprojetsup/metier/infrastructure/repository/RechercheMetierBDDRepositoryTest.kt
+++ app/back/src/test/fr/gouv/monprojetsup/metier/infrastructure/repository/RechercheMetierBDDRepositoryTest.kt
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.jdbc.Sql
 
-class RechercheMetierAvecSesFormationsBDDRepositoryTest : BDDRepositoryTest() {
+class RechercheMetierBDDRepositoryTest : BDDRepositoryTest() {
     @Autowired
     lateinit var entityManager: EntityManager
 
@@ -50,7 +50,7 @@ class RechercheMetierAvecSesFormationsBDDRepositoryTest : BDDRepositoryTest() {
                             "développement et conseil en orientation scolaire et professionnelle",
                 ),
             )
-        assertThat(result).isEqualTo(attendu)
+        assertThat(result.containsAll(attendu)).isTrue()
     }
 
     @Test
@@ -73,12 +73,12 @@ class RechercheMetierAvecSesFormationsBDDRepositoryTest : BDDRepositoryTest() {
                 MetierCourt(id = "MET_19", nom = "palefrenier / palefrenière"),
                 MetierCourt(id = "MET_98", nom = "sellier/ère"),
             )
-        assertThat(result).isEqualTo(attendu)
+        assertThat(result.containsAll(attendu)).isTrue()
     }
 
     @Test
     @Sql("classpath:recherche_metier.sql")
-    fun `Si sport, renvoyer les métiers associés`() {
+    fun `Si sport, renvoyer les métiers associés en plaçant en premier ceux contenant le mot tel quel`() {
         // Given
         val recherche = "sport"
 
@@ -91,12 +91,12 @@ class RechercheMetierAvecSesFormationsBDDRepositoryTest : BDDRepositoryTest() {
                 MetierCourt(id = "MET_392", nom = "ergonome du sport"),
                 MetierCourt(id = "MET_140", nom = "gestionnaire du sport"),
                 MetierCourt(id = "MET_522", nom = "Professeur / professeure de sport"),
-                MetierCourt(id = "MET_385", nom = "journaliste sportive"),
-                MetierCourt(id = "MET_481", nom = "entraîneur/euse sportif/ve"),
-                MetierCourt(id = "MET_783", nom = "éducateur sportif / éducatrice sportive"),
                 MetierCourt(id = "MET_820", nom = "chercheur/euse en biologie du sport"),
                 MetierCourt(id = "MET_375", nom = "masseur-kinésithérapeute (sport)"),
                 MetierCourt(id = "MET_135", nom = "vendeur/euse d'articles de sport"),
+                MetierCourt(id = "MET_385", nom = "journaliste sportive"),
+                MetierCourt(id = "MET_481", nom = "entraîneur/euse sportif/ve"),
+                MetierCourt(id = "MET_783", nom = "éducateur sportif / éducatrice sportive"),
                 MetierCourt(id = "MET_43", nom = "professeur/e d'éducation physique et sportive"),
                 MetierCourt(id = "MET_530", nom = "animateur/trice handisport"),
                 MetierCourt(id = "MET_526", nom = "designer transports"),
@@ -106,7 +106,23 @@ class RechercheMetierAvecSesFormationsBDDRepositoryTest : BDDRepositoryTest() {
                 MetierCourt(id = "MET_832", nom = "pisteur / pisteuse secouriste"),
                 MetierCourt(id = "MET_132", nom = "Médiateur social / médiatrice sociale"),
             )
-        assertThat(result).isEqualTo(attendu)
+        assertThat(result.containsAll(attendu)).isTrue()
+        val indexMET135 = result.indexOfFirst { it.id == "MET_135" }
+        val indexMET140 = result.indexOfFirst { it.id == "MET_140" }
+        val indexMET375 = result.indexOfFirst { it.id == "MET_375" }
+        val indexMET392 = result.indexOfFirst { it.id == "MET_392" }
+        val indexMET522 = result.indexOfFirst { it.id == "MET_522" }
+        val indexMET820 = result.indexOfFirst { it.id == "MET_820" }
+        val indexMetiersContenantLeMotSport =
+            listOf(
+                indexMET135,
+                indexMET140,
+                indexMET375,
+                indexMET392,
+                indexMET522,
+                indexMET820,
+            )
+        assertThat(indexMetiersContenantLeMotSport.all { it < indexMetiersContenantLeMotSport.size })
     }
 
     @Test
@@ -141,6 +157,55 @@ class RechercheMetierAvecSesFormationsBDDRepositoryTest : BDDRepositoryTest() {
                 ),
                 MetierCourt(id = "MET_7856", nom = "gérontopsychiatre"),
             )
-        assertThat(result).isEqualTo(attendu)
+        assertThat(result.containsAll(attendu)).isTrue()
+    }
+
+    @Test
+    @Sql("classpath:recherche_metier.sql")
+    fun `Si policier, renvoyer les métiers associés`() {
+        // Given
+        val recherche = "policier"
+
+        // When
+        val result = rechercheMetierBDDRepository.rechercherMetiersCourts(recherche)
+
+        // Then
+        val attendu =
+            listOf(
+                MetierCourt(id = "MET_1011", nom = "maître-chien de la Police nationale"),
+                MetierCourt(id = "MET_191", nom = "officier / officière de police"),
+                MetierCourt(id = "MET_493", nom = "motard/e de la police ou de la gendarmerie"),
+                MetierCourt(id = "MET_598", nom = "ingénieur / ingénieure de la police technique et scientifique"),
+                MetierCourt(id = "MET_786", nom = "commissaire de police"),
+                MetierCourt(id = "MET_892", nom = "technicien/ne de police technique et scientifique"),
+                MetierCourt(id = "MET_191", nom = "officier / officière de police"),
+                MetierCourt(id = "MET_427", nom = "officier marinier / officière marinière"),
+                MetierCourt(id = "MET_565", nom = "officier / officière de la marine nationale"),
+                MetierCourt(id = "MET_567", nom = "officier / officière de l'armée de terre"),
+                MetierCourt(id = "MET_653", nom = "officier / officière de la marine marchande"),
+                MetierCourt(id = "MET_683", nom = "officier / officière de l'armée de l'air"),
+                MetierCourt(id = "MET_691", nom = "officier / officière de gendarmerie"),
+                MetierCourt(id = "MET_810", nom = "démineur/euse"),
+            )
+        assertThat(result.containsAll(attendu)).isTrue()
+    }
+
+    @Test
+    @Sql("classpath:recherche_metier.sql")
+    fun `Si policier, renvoyer les métiers associés en prenant en compte les fautes`() {
+        // Given
+        val recherche = "astronote"
+
+        // When
+        val result = rechercheMetierBDDRepository.rechercherMetiersCourts(recherche)
+
+        // Then
+        val attendu =
+            listOf(
+                MetierCourt(id = "MET_7835", nom = "Astronaute"),
+                MetierCourt(id = "MET_24", nom = "astrophysicien / astrophysicienne"),
+                MetierCourt(id = "MET_24", nom = "astrophysicien / astrophysicienne"),
+            )
+        assertThat(result.containsAll(attendu)).isTrue()
     }
 }
