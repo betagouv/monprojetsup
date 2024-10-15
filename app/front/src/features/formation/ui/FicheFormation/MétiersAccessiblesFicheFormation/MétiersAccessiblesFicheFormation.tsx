57c041f416d4cf24cb3bba05529fc360f14c132c
aurelia.del.zotto <aurelia.del.zotto@octo.com>
2024-10-15 15:57:07 +0200

    feat(317) : Ajouter un coeur pour visualiser les métiers en favoris

--- app/front/src/features/formation/ui/FicheFormation/MétiersAccessiblesFicheFormation/MétiersAccessiblesFicheFormation.tsx
+++ app/front/src/features/formation/ui/FicheFormation/MétiersAccessiblesFicheFormation/MétiersAccessiblesFicheFormation.tsx
@@ -1,18 +1,43 @@
 import { type MétiersAccessiblesFicheFormationProps } from "./MétiersAccessiblesFicheFormation.interface";
 import Bouton from "@/components/Bouton/Bouton";
+import { BoutonProps } from "@/components/Bouton/Bouton.interface.tsx";
 import ListeLiensExternesSousFormeBouton from "@/components/ListeLiensExternesSousFormeBouton/ListeLiensExternesSousFormeBouton";
 import Modale from "@/components/Modale/Modale";
 import Titre from "@/components/Titre/Titre";
+import { constantes } from "@/configuration/constantes";
 import { i18n } from "@/configuration/i18n/i18n";
 import BoutonsActionsModalMétier from "@/features/métier/ui/BoutonsActionsMétier/BoutonsActionsModalMétier";
+import { élèveQueryOptions } from "@/features/élève/ui/élèveQueries";
+import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
 const MétiersAccessiblesFicheFormation = ({ métiers }: MétiersAccessiblesFicheFormationProps) => {
-  const NOMBRE_MÉTIERS_À_AFFICHER = 10;
+  const { data: élève } = useQuery(élèveQueryOptions);
 
   const [métierSélectionné, setMétierSélectionné] = useState<MétiersAccessiblesFicheFormationProps["métiers"][number]>(
     métiers[0],
   );
+
+  const afficherCoeurFavoris = (métierId: string): BoutonProps["icône"] => {
+    if (élève?.métiersFavoris?.includes(métierId)) {
+      return { position: "gauche", classe: "fr-icon-heart-fill" };
+    }
+
+    return undefined;
+  };
+
+  const métiersTriésParFavoris = [...métiers].sort((a, b) => {
+    const aEstFavori = élève?.métiersFavoris?.includes(a.id);
+    const bEstFavori = élève?.métiersFavoris?.includes(b.id);
+
+    if (aEstFavori && !bEstFavori) {
+      return -1;
+    } else if (!aEstFavori && bEstFavori) {
+      return 1;
+    }
+
+    return 0;
+  });
 
   if (métiers.length === 0) return null;
 
@@ -29,12 +54,13 @@ const MétiersAccessiblesFicheFormation = ({ métiers }: MétiersAccessiblesFich
         </Titre>
       </div>
       <ul className="flex list-none flex-wrap justify-start gap-1 p-0">
-        {métiers.slice(0, NOMBRE_MÉTIERS_À_AFFICHER).map((métier) => (
+        {métiersTriésParFavoris.slice(0, constantes.FICHE_FORMATION.NB_MÉTIERS_À_AFFICHER).map((métier) => (
           <li key={métier.id}>
             <Bouton
               ariaControls={idModale}
               auClic={() => setMétierSélectionné(métier)}
               dataFrOpened="false"
+              icône={afficherCoeurFavoris(métier.id)}
               label={métier.nom}
               taille="petit"
               type="button"
