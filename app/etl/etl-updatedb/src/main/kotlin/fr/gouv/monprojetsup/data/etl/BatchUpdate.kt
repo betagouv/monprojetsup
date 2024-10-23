52a8d7d02baba87ec182eaad5dff26f0b16dea80
Hugo Gimbert <hugo.gimbert@cnrs.fr>
2024-10-23 11:37:49 +0200

    aucune entite n'est effacée par l'etl, seulement des relations

--- app/etl/etl-updatedb/src/main/kotlin/fr/gouv/monprojetsup/data/etl/BatchUpdate.kt
+++ app/etl/etl-updatedb/src/main/kotlin/fr/gouv/monprojetsup/data/etl/BatchUpdate.kt
@@ -10,26 +10,14 @@ class BatchUpdate(
 ) {
 
     fun <T> setEntities(entityName: String, entities: Collection<T> ) {
-
-        clearEntities(entityName)
-        addEntities(entities)
-
-    }
-
-    fun <T> addEntities(entities: Collection<T>) {
         val statelessSession: StatelessSession = sessionFactory.openStatelessSession()
         val transaction: Transaction = statelessSession.beginTransaction()
-        entities.forEach{ statelessSession.insert(it)}
-        transaction.commit()
-        statelessSession.close()
-    }
 
-    fun clearEntities(entityName: String) {
-        val statelessSession: StatelessSession = sessionFactory.openStatelessSession()
-        val transaction: Transaction = statelessSession.beginTransaction()
         val hql = "DELETE FROM $entityName"
         val query = statelessSession.createMutationQuery(hql)
         query.executeUpdate()
+
+        entities.forEach{ statelessSession.insert(it)}
 
         transaction.commit()
         statelessSession.close()
@@ -39,14 +27,6 @@ class BatchUpdate(
         val statelessSession: StatelessSession = sessionFactory.openStatelessSession()
         val transaction: Transaction = statelessSession.beginTransaction()
         entities.forEach{ statelessSession.upsert(it)}
-        transaction.commit()
-        statelessSession.close()
-    }
-
-    fun <T> deleteEntities(entities: Collection<T>) {
-        val statelessSession: StatelessSession = sessionFactory.openStatelessSession()
-        val transaction: Transaction = statelessSession.beginTransaction()
-        entities.forEach{ statelessSession.delete(it)}
         transaction.commit()
         statelessSession.close()
     }
