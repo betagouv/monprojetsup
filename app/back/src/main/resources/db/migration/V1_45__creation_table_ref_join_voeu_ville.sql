47a62bccdd3049bd9c50cc8e2a3da261b4a64652
Hugo Gimbert <hugo.gimbert@cnrs.fr>
2025-03-02 00:57:03 +0100

    creatin seq for paniers voeux

--- app/back/src/main/resources/db/migration/V1_45__creation_table_ref_join_voeu_ville.sql
+++ app/back/src/main/resources/db/migration/V1_45__creation_table_ref_join_voeu_ville.sql
@@ -1,5 +1,5 @@
 ALTER TABLE sugg_paniers_voeux ALTER COLUMN id SET DEFAULT 0;
-DROP SEQUENCE sugg_paniers_voeux_seq;
+DROP SEQUENCE IF EXISTS sugg_paniers_voeux_seq;
 CREATE SEQUENCE sugg_paniers_voeux_seq
     START WITH 1
     INCREMENT BY 50
