c42b10853ee08146e12bf2980514642b6df164db
Hugo Gimbert <hugo.gimbert@cnrs.fr>
2025-03-12 00:29:16 +0100

    forces reload of main page after auth

--- app/front/src/routes/_main.tsx
+++ app/front/src/routes/_main.tsx
@@ -1,4 +1,5 @@
 import MainLayout from "@/components/_layout/MainLayout/MainLayout";
+import { élèveQueryOptions } from "@/features/élève/ui/élèveQueries";
 import { référentielDonnéesQueryOptions } from "@/features/référentielDonnées/ui/référentielDonnéesQueries";
 import { type QueryClient } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
@@ -6,6 +7,7 @@ import { z } from "zod";
 
 const chargerDonnées = async (queryClient: QueryClient) => {
   await queryClient.ensureQueryData(référentielDonnéesQueryOptions);
+  await queryClient.ensureQueryData(élèveQueryOptions);
 };
 
 const tableauDeBordSearchSchema = z.object({
@@ -15,7 +17,11 @@ const tableauDeBordSearchSchema = z.object({
 export const Route = createFileRoute("/_main")({
   validateSearch: (searchParamètres) => tableauDeBordSearchSchema.parse(searchParamètres),
   component: MainLayout,
-  loader: async ({ context: { queryClient } }) => {
+  loader: async ({ context: { queryClient, auth } }) => {
     await chargerDonnées(queryClient);
+    auth.events.addUserLoaded(async () => {
+      await queryClient.refetchQueries(élèveQueryOptions);
+      window.location.href = "/";
+    });
   },
 });
--- app/front/src/routes/connexion/index.tsx
+++ app/front/src/routes/connexion/index.tsx
@@ -1,25 +1,8 @@
 import MainLayout from "@/components/_layout/MainLayout/MainLayout";
 import { environnement } from "@/configuration/environnement";
-import { élèveQueryOptions } from "@/features/élève/ui/élèveQueries";
-import { référentielDonnéesQueryOptions } from "@/features/référentielDonnées/ui/référentielDonnéesQueries";
-import { type QueryClient } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import { withAuthenticationRequired } from "react-oidc-context";
 
-const chargerDonnées = async (queryClient: QueryClient) => {
-  await queryClient.ensureQueryData(référentielDonnéesQueryOptions);
-  await queryClient.ensureQueryData(élèveQueryOptions);
-};
-
 export const Route = createFileRoute("/connexion/")({
   component: environnement.VITE_TEST_MODE ? MainLayout : withAuthenticationRequired(MainLayout),
-  loader: async ({ context: { queryClient, auth } }) => {
-    const user = await auth.getUser();
-
-    if (user?.expires_in && user.expires_in >= 5) {
-      await chargerDonnées(queryClient);
-    } else {
-      auth.events.addUserLoaded(async () => await chargerDonnées(queryClient));
-    }
-  },
 });
