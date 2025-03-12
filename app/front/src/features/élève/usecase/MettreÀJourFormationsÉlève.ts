5fd5d5cfd9d5db8db30a72083bf78eb71ece7a7c
Hugo Gimbert <hugo.gimbert@cnrs.fr>
2025-03-12 16:08:08 +0100

    ajout des labels dans les remontées matomo de favoris formations et métiers

--- app/front/src/features/élève/usecase/MettreÀJourFormationsÉlève.ts
+++ app/front/src/features/élève/usecase/MettreÀJourFormationsÉlève.ts
@@ -1,24 +1,33 @@
 import { type Élève } from "@/features/élève/domain/élève.interface";
 import { type ÉlèveRepository } from "@/features/élève/infrastructure/gateway/élèveRepository.interface";
 import { Formation } from "@/features/formation/domain/formation.interface.ts";
+import { FormationRepository } from "@/features/formation/infrastructure/formationRepository.interface";
 import { AnalyticsRepository } from "@/services/analytics/analytics.interface";
 
 export class MettreÀJourFormationsÉlèveUseCase {
   public constructor(
     private readonly _élèveRepository: ÉlèveRepository,
+    private readonly _formationRepository: FormationRepository,
     private readonly _analytics: AnalyticsRepository,
   ) {}
 
   public async run(élève: Élève, idsFormationsÀModifier: Formation["id"][]): Promise<Élève | Error> {
     const formations = new Set(élève.formations);
 
-    for (const idFormation of idsFormationsÀModifier) {
+    const réponse = await this._formationRepository.récupérerPlusieursFiches(idsFormationsÀModifier);
+    if (réponse instanceof Error) {
+      return réponse;
+    }
+
+    for (const fiche of réponse) {
+      const idFormation = fiche.id;
+      const label = fiche.nom + " (" + fiche.id + ")";
       if (formations.has(idFormation)) {
         formations.delete(idFormation);
-        this._analytics.envoyerÉvènement("Formation Favorite", "Supprimer", idFormation);
+        this._analytics.envoyerÉvènement("Formation Favorite", "Supprimer", label);
       } else {
         formations.add(idFormation);
-        this._analytics.envoyerÉvènement("Formation Favorite", "Ajouter", idFormation);
+        this._analytics.envoyerÉvènement("Formation Favorite", "Ajouter", label);
       }
     }
 
--- app/front/src/features/élève/usecase/MettreÀJourMétiersÉlève.ts
+++ app/front/src/features/élève/usecase/MettreÀJourMétiersÉlève.ts
@@ -1,23 +1,32 @@
 import { type Élève, MétierÉlève } from "@/features/élève/domain/élève.interface";
 import { type ÉlèveRepository } from "@/features/élève/infrastructure/gateway/élèveRepository.interface";
+import { type MétierRepository } from "@/features/métier/infrastructure/métierRepository.interface";
 import { AnalyticsRepository } from "@/services/analytics/analytics.interface";
 
 export class MettreÀJourMétiersÉlèveUseCase {
   public constructor(
     private readonly _élèveRepository: ÉlèveRepository,
+    private readonly _métierRepository: MétierRepository,
     private readonly _analytics: AnalyticsRepository,
   ) {}
 
   public async run(élève: Élève, idsMétiersÀModifier: MétierÉlève[]): Promise<Élève | Error> {
     const métiers = new Set(élève.métiersFavoris);
 
-    for (const idMétier of idsMétiersÀModifier) {
+    const réponse = await this._métierRepository.récupérerPlusieurs(idsMétiersÀModifier);
+    if (réponse instanceof Error) {
+      return réponse;
+    }
+
+    for (const métier of réponse) {
+      const idMétier = métier.id;
+      const label = métier.nom + " (" + idMétier + ")";
       if (métiers.has(idMétier)) {
         métiers.delete(idMétier);
-        this._analytics.envoyerÉvènement("Métier Favori", "Supprimer", idMétier);
+        this._analytics.envoyerÉvènement("Métier Favori", "Supprimer", label);
       } else {
         métiers.add(idMétier);
-        this._analytics.envoyerÉvènement("Métier Favori", "Ajouter", idMétier);
+        this._analytics.envoyerÉvènement("Métier Favori", "Ajouter", label);
       }
     }
 
