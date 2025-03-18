9d54c85983a0783aec1aa2efeef8af97f7e71bd7
kevin.rifa <kevin.rifa@onisep.fr>
2025-03-18 15:43:05 +0100

    #15825 - Poupup Button

--- app/front/src/features/formation/infrastructure/gateway/formationHttpRepository/formationHttpRepository.ts
+++ app/front/src/features/formation/infrastructure/gateway/formationHttpRepository/formationHttpRepository.ts
@@ -232,11 +232,11 @@ export class formationHttpRepository implements FormationRepository {
         })) ?? [],
       duréeÉtudesPrévue: explications.dureeEtudesPrevue ?? null,
       alternance: explications.alternance ?? null,
-      choixÉlève:
-        explications.choixEleve?.map((choix) => ({
+      choixÉlève: Array.isArray(explications?.choixEleve)
+        ? explications.choixEleve.map((choix) => ({
           id: choix.id,
           nom: choix.nom,
-        })) ?? [],
+        })) : [],
       spécialitésChoisies: explications.specialitesChoisies.map((spécialité) => ({
         nom: spécialité.nomSpecialite,
         pourcentageAdmisAnnéePrécédente: spécialité.pourcentage,
