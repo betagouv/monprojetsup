6a718253776972c4214ea74b0556e228ac302576
Hugo Gimbert <hugo.gimbert@cnrs.fr>
2025-04-24 16:04:53 +0200

    insertion profils de reference

---
+++ app/etl/etl-common/src/main/kotlin/fr/gouv/monprojetsup/data/suggestions/entity/profil/FormationFavoriteEntity.kt
@@ -0,0 +1,7 @@
+package fr.gouv.monprojetsup.data.suggestions.entity.profil
+
+data class FormationFavoriteEntity(
+    val idFormation: String,
+    val niveauAmbition: Int,
+    val priseDeNote: String?,
+)
