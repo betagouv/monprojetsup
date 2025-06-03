7b6c725d3ee5917f20cbd793f8cb5e083f64a0b6
kevin.rifa <kevin.rifa@onisep.fr>
2025-06-03 15:15:17 +0200

    #15825 - ParcourSup Btn Integration

--- app/front/src/components/SélecteurFavoris/Favori/Favori.tsx
+++ app/front/src/components/SélecteurFavoris/Favori/Favori.tsx
@@ -65,7 +65,7 @@ const Favori = ({
       </Toggle>
 
       <button
-        className="fr-btn--tertiary p-2 fr-mx-1-5v"
+        className="fr-btn--tertiary fr-mx-1-5v p-2"
         type="button"
       >
         <img
