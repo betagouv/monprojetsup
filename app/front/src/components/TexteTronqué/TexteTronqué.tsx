c1ff85db932b7095d72b463f86979245b6baa911
kevin.rifa <kevin.rifa@onisep.fr>
2025-06-03 15:39:03 +0200

    #16067 - Lint corrections

--- app/front/src/components/TexteTronqué/TexteTronqué.tsx
+++ app/front/src/components/TexteTronqué/TexteTronqué.tsx
@@ -1,31 +1,15 @@
 import { type TexteTronquéProps } from "./TexteTronqué.interface";
-import { i18n } from "@/configuration/i18n/i18n";
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 
 const TexteTronqué = ({ texte }: TexteTronquéProps) => {
-  const [afficherEnEntier, setAfficherEnEntier] = useState(false);
-  const [afficherBoutonLireLaSuite, setAfficherBoutonLireLaSuite] = useState(false);
-
   const ref = useRef<HTMLParagraphElement | null>(null);
-
-  useEffect(() => {
-    const doitAfficherBoutonLireLaSuite =
-      ref?.current?.offsetHeight &&
-      ref?.current?.scrollHeight &&
-      ref?.current?.offsetWidth &&
-      ref?.current?.scrollWidth &&
-      (ref?.current?.offsetHeight < ref?.current?.scrollHeight ||
-        ref?.current?.offsetWidth < ref?.current?.scrollWidth);
-
-    setAfficherBoutonLireLaSuite(Boolean(doitAfficherBoutonLireLaSuite));
-  }, []);
 
   if (!texte || texte === "") return null;
 
   return (
     <div className="justify-start">
       <p
-        className={`mb-2 whitespace-pre-line`}
+        className="mb-2 whitespace-pre-line"
         ref={ref}
       >
         {texte}
