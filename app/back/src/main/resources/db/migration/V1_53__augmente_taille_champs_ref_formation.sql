e7077830e9765f45f2f0d59d9852fcb29ea13cf8
Hugo Gimbert <hugo.gimbert@cnrs.fr>
2025-07-20 14:35:49 +0200

    migration schema pou augmentation taille label dans ref_formation

---
+++ app/back/src/main/resources/db/migration/V1_53__augmente_taille_champs_ref_formation.sql
@@ -0,0 +1,24 @@
+DROP VIEW expanded_label;
+DROP VIEW expanded_keywords;
+
+ALTER TABLE ref_formation ALTER COLUMN label_details TYPE VARCHAR;
+ALTER TABLE ref_formation ALTER COLUMN label TYPE character varying (2053);
+
+CREATE OR REPLACE VIEW expanded_keywords AS
+SELECT distinct id,
+                label,
+                                  unaccent(lower(trim(word))) AS mot_clef
+                           FROM ref_formation r,
+                           unnest(mots_clefs) AS mot_clef,
+                           unnest(string_to_array(mot_clef, ' ')) AS word
+                           WHERE r.obsolete = false AND LENGTH(trim(word)) >= 2;
+
+CREATE OR REPLACE VIEW expanded_label AS
+SELECT distinct
+        id,
+        label,
+        unaccent(lower(trim(label))) AS label_sans_accents,
+        unaccent(lower(trim(label_decoupe_brut))) AS label_decoupe
+FROM ref_formation,
+       unnest(string_to_array(regexp_replace(label, '[^0-9A-Za-zÀ-ÖØ-öø-ÿ]+', ' ', 'g'), ' ')) AS label_decoupe_brut
+WHERE LENGTH(label_decoupe_brut) >= 2 and obsolete = false;
