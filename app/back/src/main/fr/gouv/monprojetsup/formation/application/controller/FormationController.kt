48623e23ece0cb8814b521bda4048a65a9e19b5c
Jérome <jerome.augustin@onisep.fr>
2025-07-24 11:28:16 +0200

    #15825 - fix merge

--- app/back/src/main/fr/gouv/monprojetsup/formation/application/controller/FormationController.kt
+++ app/back/src/main/fr/gouv/monprojetsup/formation/application/controller/FormationController.kt
@@ -67,6 +67,7 @@ class FormationController(
                 request.profil == null -> recupererEleveAvecProfilExistant() ?: AvecProfilExistant("")
                 else -> request.profil.toProfilExistant()
             }
+
         val suggestions = suggestionsFormationsService.recupererLesSuggestionsPourUnProfil(profilEleve)
         val hateoas =
             hateoasBuilder.creerHateoas(
