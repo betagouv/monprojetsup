54380dac6b693fde27ffb1f9fee99747ff384b21
kevin.rifa <kevin.rifa@onisep.fr>
2025-10-29 16:46:43 +0100

    Change HomeLink Params Href

--- app/front/src/components/_layout/useLayout.tsx
+++ app/front/src/components/_layout/useLayout.tsx
@@ -24,7 +24,7 @@ export default function useLayout() {
   );
 
   const lienAccueil: HeaderProps["homeLinkProps"] = {
-    to: "https://www.onisep.fr",
+    href: "https://www.onisep.fr",
     title: `ONISEP`,
   };
 
