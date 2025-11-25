224086c8283fc6f2d808ba1644a639a089d00062
Florian BOUCHÉ <florian.bouche@onisep.fr>
2025-11-25 11:31:16 +0100

    Revert "Merge branch 'fix_code' into 'develop'"
    This reverts merge request !8

--- app/front/src/components/SélecteurFavoris/Favori/Favori.interface.ts
+++ app/front/src/components/SélecteurFavoris/Favori/Favori.interface.ts
@@ -12,7 +12,7 @@ export type Favori = {
   callbackMettreÀJour?: (idFavori: string) => Promise<unknown>;
   idDeLonglet?: string;
   parcoursupSynchronizable?: boolean;
-  SyncAvecParcoursup?: boolean;
+  isParcoursupSync?: boolean;
 };
 
 export type FavoriProps = Favori;
--- app/front/src/features/formation/ui/CommunesProposantLaFormation/CommunesProposantLaFormation.tsx
+++ app/front/src/features/formation/ui/CommunesProposantLaFormation/CommunesProposantLaFormation.tsx
@@ -21,20 +21,14 @@ const CommunesProposantLaFormation = ({ communes, lienParcoursSup }: CommunesPro
           {communes && communes.length > 0 ? (
             <>
               {" "}
-              {communes.length}{" "}
-              {communes.length > 1
-                ? i18n.PAGE_FORMATION.COMMUNES_PROPOSANT_FORMATION_SUITE_PLURIEL
-                : i18n.PAGE_FORMATION.COMMUNES_PROPOSANT_FORMATION_SUITE_SINGULIER}{" "}
+              {communes.length} {communes.length > 1 ? i18n.PAGE_FORMATION.COMMUNES_PROPOSANT_FORMATION_SUITE_PLURIEL : i18n.PAGE_FORMATION.COMMUNES_PROPOSANT_FORMATION_SUITE_SINGULIER}{" "}
               {i18n.PAGE_FORMATION.COMMUNES_PROPOSANT_FORMATION_SUITE_SI_CORRESPONDANCE}{" "}
               <strong>{communes.slice(0, 3).join(" • ")}</strong>
             </>
           ) : (
             <strong>
               {" "}
-              {communes.length}{" "}
-              {communes.length > 1
-                ? i18n.PAGE_FORMATION.COMMUNES_PROPOSANT_FORMATION_SUITE_PLURIEL
-                : i18n.PAGE_FORMATION.COMMUNES_PROPOSANT_FORMATION_SUITE_SINGULIER}
+              {communes.length} {communes.length > 1 ? i18n.PAGE_FORMATION.COMMUNES_PROPOSANT_FORMATION_SUITE_PLURIEL : i18n.PAGE_FORMATION.COMMUNES_PROPOSANT_FORMATION_SUITE_SINGULIER}
             </strong>
           )}
         </p>
