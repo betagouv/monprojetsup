1e659fcd763a2eac91b47bd307aa0e25d5918ce0
kevin.rifa <kevin.rifa@onisep.fr>
2025-11-28 10:23:04 +0100

    #16420 - Check user Auth

--- app/front/src/components/SélecteurFavoris/MaSélectionFavoris/MaSélectionFavoris.tsx
+++ app/front/src/components/SélecteurFavoris/MaSélectionFavoris/MaSélectionFavoris.tsx
@@ -2,15 +2,17 @@ import { MaSélectionFavorisProps } from "./MaSélectionFavoris.interface";
 import ListeDeFavoris from "@/components/SélecteurFavoris/ListeDeFavoris/ListeDeFavoris";
 import { i18n } from "@/configuration/i18n/i18n";
 import { useMemo } from "react";
+import useUtilisateur from "@/features/utilisateur/ui/useUtilisateur.ts";
 
 const MaSélectionFavoris = ({ favoris, messageAucun }: MaSélectionFavorisProps) => {
   const favorisSélectionnés = useMemo(() => favoris.filter((favori) => favori.estFavori), [favoris]);
+  const utilisateur = useUtilisateur();
 
   return (
     <div>
       <p className="mb-0 font-bold text-[--text-label-grey]">{i18n.COMMUN.FAVORIS.MA_SÉLÉCTION}</p>
       {favorisSélectionnés.length > 0 ? (
-        <ListeDeFavoris favoris={favorisSélectionnés} parcoursupSynchronizable={true} />
+        <ListeDeFavoris favoris={favorisSélectionnés} parcoursupSynchronizable={utilisateur.estAuthentifié} />
       ) : (
         <p className="fr-text--sm mb-0">{messageAucun}</p>
       )}
