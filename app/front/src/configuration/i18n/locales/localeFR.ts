2c39f571380b70f6130123ddeec53aa0c3fbb154
gibril.khelk <gibril.khelk@onisep.fr>
2025-12-02 16:26:28 +0100

    #16408 - fix wording again

--- app/front/src/configuration/i18n/locales/localeFR.ts
+++ app/front/src/configuration/i18n/locales/localeFR.ts
@@ -395,7 +395,7 @@ export const localeFR = {
             "Une fois précisés ton profil et tes préférences, des suggestions personnalisées te seront proposées.",
         },
         CONNECTE_TOI: {
-          TITRE: "MPS est un outil de la plateforme Avenirs(s).",
+          TITRE: "MPS est un outil de la plateforme Avenir(s).",
           SOUS_TITRE: "En te connectant, tu conserveras tes préférences et tes favoris.",
         },
         AVIS: {
