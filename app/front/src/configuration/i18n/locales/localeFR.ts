5cadae8d2fbc786f9ec22af37124213a208dd71a
kevin.rifa <kevin.rifa@onisep.fr>
2025-12-03 16:27:50 +0100

    #16439 - Parcoursup block sync

--- app/front/src/configuration/i18n/locales/localeFR.ts
+++ app/front/src/configuration/i18n/locales/localeFR.ts
@@ -405,11 +405,14 @@ export const localeFR = {
         },
         PARCOURSUP: {
           TITRE: "Tu as déjà un compte Parcoursup ?",
-          SOUS_TITRE: "En synchronisant MPS avec Parcoursup, tu retrouveras dans MPS les formations que tu as déjà mises en favori dans Parcoursup.",
+          SOUS_TITRE:
+            "En synchronisant MPS avec Parcoursup, tu retrouveras dans MPS les formations que tu as déjà mises en favori dans Parcoursup.",
           BOUTON: "Synchroniser",
           ALT_ILLUSTRATION: "MPS & ParcourSup (entrez dans l'enseignement supérieur)",
           SI_SYNCHRO: {
-            SOUS_TITRE: "Tes comptes sont bien synchronisés. Cette autorisation est revocable en écrivant à",
+            TITRE: "Tes comptes MPS et Parcoursup sont synchronisés",
+            SOUS_TITRE: "En désynchronisant tes comptes, tes favoris Parcoursup ne seront plus importés",
+            BOUTON: "Désynchroniser",
           },
         },
       },
--- app/front/src/features/élève/ui/TableauDeBordÉlèvePage/CarteSecondaireTableauDeBordÉlève/CarteSecondaireTableauDeBordÉlève.tsx
+++ app/front/src/features/élève/ui/TableauDeBordÉlèvePage/CarteSecondaireTableauDeBordÉlève/CarteSecondaireTableauDeBordÉlève.tsx
@@ -19,7 +19,7 @@ const CarteSecondaireTableauDeBordÉlève = ({
           <p className="fr-h3 mb-2 text-[--text-active-blue-france]">{titre}</p>
           <p className="fr-text--lead mb-0">{sousTitre}</p>
         </div>
-        <div className="pt-2 ml-auto">{children}</div>
+        <div className="ml-auto pt-2">{children}</div>
       </div>
     </div>
   );
