c9b43e97018eed212cc832dcfdb70818f53ebe7e
Hugo Gimbert <hugo.gimbert@cnrs.fr>
2026-02-11 15:00:11 +0100

    added missing files

---
+++ app/back/src/main/fr/gouv/monprojetsup/eleve/application/dto/ProgressionDTO.kt
@@ -0,0 +1,14 @@
+package fr.gouv.monprojetsup.eleve.application.dto
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.v3.oas.annotations.media.Schema
+
+data class ProgressionDTO(
+    @JsonProperty("progression")
+    @Schema(
+        description = "Progression dans les six niveaux MPS",
+        example = "6",
+        allowableValues = ["0", "1", "2", "3", "4", "5", "6"],
+    )
+    val progression: Int,
+)
