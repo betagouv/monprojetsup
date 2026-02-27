45752b0fca501ed6c80991a5990d6f30c1f2fb2b
Hugo Gimbert <hugo.gimbert@cnrs.fr>
2026-02-27 07:34:52 +0100

    verbose claims

--- app/back/src/main/fr/gouv/monprojetsup/authentification/application/controller/AuthentifieController.kt
+++ app/back/src/main/fr/gouv/monprojetsup/authentification/application/controller/AuthentifieController.kt
@@ -32,9 +32,19 @@ abstract class AuthentifieController {
 
         if (authentification is JwtAuthenticationToken) {
             val jwt = authentification.getToken()
-
             return jwt.hasClaim("EXPERT")
         }
         return false
+    }
+
+    @Throws(MonProjetSupForbiddenException::class)
+    protected fun getAllClaims(): String {
+        val authentification = SecurityContextHolder.getContext().authentication
+
+        if (authentification is JwtAuthenticationToken) {
+            val jwt = authentification.getToken()
+            return jwt.claims.map { it.toString() }.joinToString(" | ")
+        }
+        return ""
     }
 }
